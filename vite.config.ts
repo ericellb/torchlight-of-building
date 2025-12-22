10d49b4811ec5aa3ef699b83ef4552be44ee7982
aclghrand <aclghrand@gmail.com>
2025-12-22 12:43:10 -0800

    update cf vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    cloudflare({ viteEnvironment: { name: "client" } }),
     tsconfigPaths(),
     tanstackStart(),
     react(),
